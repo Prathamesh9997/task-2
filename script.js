81bd1b5b4641bab700bd8d09b2c25e46bfedae64
Prathamesh Gunde <65587388+Prathamesh9997@users.noreply.github.com>
2021-01-11 14:28:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//1. Create a request variable
+var request = new XMLHttpRequest();
+//2. Create a connection
+request.open('GET', 'https://restcountries.eu/rest/v2/all', true);
+//3. Send a request
+request.send();
+//4. Load the response
+request.onload = function(){
+    var data = JSON.parse(this.response);
+    for(var i in data)
+    console.log(data[i].name);
+}
+
